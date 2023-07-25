c6d855b59671617be93e4ef1f4d146f13bfe4215
Vikash Shukla <vikash.shukla@decimalpointanalytics.info>
2023-07-25 21:41:27 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c7791d955e0b36459a0f1583e366f178",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
